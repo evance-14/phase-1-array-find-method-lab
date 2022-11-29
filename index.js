58036512de0cee57c3e2efdcaa07cf9063a705f4
evance-14 <kiropevans097@gmail.com>
2022-11-29 19:50:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(arr){
+    let win = arr.find(arr => arr.result === "W");
+    if(win){
+        return win.year;
+    }
+}
+console.log(superbowlWin(record))
+
+
+    
+        
+    
+
